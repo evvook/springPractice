0f1906bc005ff3529b137bd708f9db85c37debf4
evvook <sthalunar@gmail.com>
2022-08-30 11:49:32 +0900

    1. 로그인 컨트롤러를 컨피그로 대체 2. 설정을 별도의 패키지로 뺌 3. war로 빌드 가능하도록 의존성 수정 및 MrsApplication클래스 수정

--- src/main/java/mrs/MrsApplication.java
+++ src/main/java/mrs/MrsApplication.java
@@ -2,10 +2,17 @@ package mrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MrsApplication {
+public class MrsApplication extends SpringBootServletInitializer{
 
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MrsApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MrsApplication.class, args);
 	}
